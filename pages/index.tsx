95a86c96a7d1804bc5c255dd8ff8bb4873638741
Bodega <Bodege@users.noreply.github.com>
2023-08-20 14:12:16 -0800

    1st Push
    BPS LETS GOOOO
    (added company name to title tag line 20

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
   return (
     <>
       <Head>
-        <title>{EnvVars.SITE_NAME}</title>
+        <title>{EnvVars.BluePherret}</title>
         <meta
           name="description"
           content="Tempor nostrud velit fugiat nostrud duis incididunt Lorem deserunt est tempor aute dolor ad elit."
